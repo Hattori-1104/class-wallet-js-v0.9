465913e3597366121f92c9e0fac64be8274368a2
HattoriNote <crafter.alloy@gmail.com>
2025-02-23 13:42:37 +0900

    sessionのフラッシュを新しい形式に

--- app/routes/invitation.$partId.tsx
+++ app/routes/invitation.$partId.tsx
@@ -1,6 +1,6 @@
 import { type LoaderFunctionArgs, redirect } from "@remix-run/node"
 import { prisma } from "~/service.server/repository"
-import { commitSession, getSession } from "~/service.server/session"
+import { commitSession, commitToastByCase, getSession } from "~/service.server/session"
 
 export const loader = async ({ request, params }: LoaderFunctionArgs) => {
   // パラメーターを取得
@@ -13,28 +13,26 @@ export const loader = async ({ request, params }: LoaderFunctionArgs) => {
 
   // ログインしていない場合はログインページへリダイレクト
   if (userId === undefined || userType !== "student") {
-    session.flash("toast", { state: "error", msg: "ログインしてください" })
     return redirect("/auth/student/login", {
-      headers: { "Set-Cookie": await commitSession(session) },
+      headers: { "Set-Cookie": await commitToastByCase(session, "UnAuthorized") },
     })
   }
 
   // パートが見つからない場合はエラーメッセージを表示してリダイレクト
   if (partId === undefined) {
-    session.flash("toast", { state: "error", msg: "パートが見つかりません" })
     return redirect("/student", {
-      headers: { "Set-Cookie": await commitSession(session) },
+      headers: { "Set-Cookie": await commitToastByCase(session, "NotFound") },
     })
   }
 
   // 会計担当者の値が不正な場合は警告メッセージを表示してリダイレクト
   if (![0, 1].includes(accountant)) {
-    session.flash("toast", { state: "warning", msg: "会計担当者の値が不正です" })
     return redirect("/student", {
-      headers: { "Set-Cookie": await commitSession(session) },
+      headers: { "Set-Cookie": await commitToastByCase(session, "NotAccountant") },
     })
   }
 
+  // ここエラーハンドリングしてないです！！！！
   // ユーザーのパート情報を作成
   await prisma.userPart.create({
     data: {
